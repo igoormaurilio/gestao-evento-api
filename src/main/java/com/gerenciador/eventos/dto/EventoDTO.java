0639aaacc8e28e7494462ea47d4d40b1eb19a0fa
Júlio <ana01cardoso02@gmail.com>
2025-05-27 09:03:58 -0300

    fix(dto): add missing id field to EventoDTO

--- src/main/java/com/gerenciador/eventos/dto/EventoDTO.java
+++ src/main/java/com/gerenciador/eventos/dto/EventoDTO.java
@@ -4,71 +4,98 @@ import java.sql.Date;
 
 import com.gerenciador.eventos.entities.Eventos;
 
-public class EventoDTO {
-		private String nome; 
-		private String descricao; 
-		private Date data;
-		private String local;
-		private int vagas;
-		
-		public EventoDTO() {
-		}
+public class EventoDTO 
+{
 
-		public EventoDTO(String nome, String descricao, Date data, String local, int vagas) {
-			this.nome = nome;
-			this.descricao = descricao;
-			this.data = data;
-			this.local = local;
-			this.vagas = vagas;
-		}
-		public EventoDTO(Eventos evento) {
-			this.nome = evento.getNome();
-			this.descricao = evento.getDescricao();
-			this.data = evento.getData();
-			this.local = evento.getLocal();
-			this.vagas = evento.getVagas();
-			
-		}
+    private Long id;        
+    private String nome;
+    private String descricao;
+    private Date data;
+    private String local;
+    private int vagas;
 
-		public String getNome() {
-			return nome;
-		}
+    public EventoDTO() 
+    {
+    	
+    }
 
-		public void setNome(String nome) {
-			this.nome = nome;
-		}
+    public EventoDTO(Long id, String nome, String descricao, Date data, String local, int vagas) 
+    {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.data = data;
+        this.local = local;
+        this.vagas = vagas;
+    }
 
-		public String getDescricao() {
-			return descricao;
-		}
+    public EventoDTO(Eventos evento) 
+    {
+        this.id = evento.getId();        
+        this.nome = evento.getNome();
+        this.descricao = evento.getDescricao();
+        this.data = evento.getData();
+        this.local = evento.getLocal();
+        this.vagas = evento.getVagas();
+    }
 
-		public void setDescricao(String descricao) {
-			this.descricao = descricao;
-		}
+    public Long getId() 
+    {
+        return id;
+    }
 
-		public Date getData() {
-			return data;
-		}
+    public void setId(Long id) 
+    {
+        this.id = id;
+    }
 
-		public void setData(Date data) {
-			this.data = data;
-		}
+    public String getNome() 
+    {
+        return nome;
+    }
 
-		public String getLocal() {
-			return local;
-		}
+    public void setNome(String nome) 
+    {
+        this.nome = nome;
+    }
 
-		public void setLocal(String local) {
-			this.local = local;
-		}
+    public String getDescricao() 
+    {
+        return descricao;
+    }
 
-		public int getVagas() {
-			return vagas;
-		}
+    public void setDescricao(String descricao) 
+    {
+        this.descricao = descricao;
+    }
 
-		public void setVagas(int vagas) {
-			this.vagas = vagas;
-		}
-		
-	}
+    public Date getData() 
+    {
+        return data;
+    }
 
+    public void setData(Date data) 
+    {
+        this.data = data;
+    }
+
+    public String getLocal() 
+    {
+        return local;
+    }
+
+    public void setLocal(String local) 
+    {
+        this.local = local;
+    }
+
+    public int getVagas() 
+    {
+        return vagas;
+    }
+
+    public void setVagas(int vagas) 
+    {
+        this.vagas = vagas;
+    }
+}
