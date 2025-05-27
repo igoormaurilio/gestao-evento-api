0d56304c3fb2a3fa7a5085086007ae7926c9bc4d
Júlio <ana01cardoso02@gmail.com>
2025-05-27 09:08:01 -0300

    feat(service): refactor code

--- src/main/java/com/gerenciador/eventos/service/ParticipanteService.java
+++ src/main/java/com/gerenciador/eventos/service/ParticipanteService.java
@@ -4,59 +4,65 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.gerenciador.eventos.dto.ParticipanteDTO;
 import com.gerenciador.eventos.entities.Participantes;
 import com.gerenciador.eventos.repository.ParticipanteRepository;
 
-public class ParticipanteService {
-	
-	@Autowired
-	ParticipanteRepository participanteRepository;
-	
-	public ParticipanteDTO insert(ParticipanteDTO dto) {
-	      Participantes entity = new Participantes();
-	      entity.setNome(dto.getNome());
-	      entity.setEmail(dto.getEmail());;
-	      entity.setTelefone(dto.getTelefone());;
-	      
-	     
-	      entity = participanteRepository.save(entity);
-	      return new ParticipanteDTO(entity);
-	  }
-	
-	   public List<ParticipanteDTO> findAll() {
-	        List<Participantes> lista = participanteRepository.findAll();
-	        return lista.stream().map(x -> new ParticipanteDTO(x)).toList();
-	    }
-	   public ParticipanteDTO update(Long id, ParticipanteDTO dto) {
-	        Optional<Participantes> optionalProduto = participanteRepository.findById(id);
-	        
-	        if (optionalProduto.isPresent()) {
-	            Participantes entity = optionalProduto.get();
-	            entity.setNome(dto.getNome());
-	            entity.setEmail(dto.getEmail());
-	            entity.setTelefone(dto.getTelefone());
-	          
-	            
-	            entity = participanteRepository.save(entity);
-	            return new ParticipanteDTO(entity);
-	        
-	        }
-	        return null;
-	        
-	    }
-	   public boolean delete(Long id) {
-	        Optional<Participantes> optionalProduto = participanteRepository.findById(id);
-	        
-	        if (optionalProduto.isPresent()) {
-	            participanteRepository.delete(optionalProduto.get());
-	            return true;
-	        }
-	        
-	        return false;
-	    }
- 
+@Service
+public class ParticipanteService 
+{
+
+    @Autowired
+    private ParticipanteRepository participanteRepository;
+
+    public ParticipanteDTO insert(ParticipanteDTO dto) 
+    {
+        Participantes entity = new Participantes();
+        entity.setNome(dto.getNome());
+        entity.setEmail(dto.getEmail());
+        entity.setTelefone(dto.getTelefone());
+
+        entity = participanteRepository.save(entity);
+        return new ParticipanteDTO(entity);
+    }
+
+    public List<ParticipanteDTO> findAll() 
+    {
+        List<Participantes> lista = participanteRepository.findAll();
+        return lista.stream()
+                    .map(ParticipanteDTO::new)
+                    .toList();
+    }
+
+    public ParticipanteDTO update(Long id, ParticipanteDTO dto) 
+    {
+        Optional<Participantes> optionalParticipante = participanteRepository.findById(id);
+
+        if (optionalParticipante.isPresent()) 
+        {
+            Participantes entity = optionalParticipante.get();
+            entity.setNome(dto.getNome());
+            entity.setEmail(dto.getEmail());
+            entity.setTelefone(dto.getTelefone());
+            entity = participanteRepository.save(entity);
+            return new ParticipanteDTO(entity);
+        }
+
+        return null;
+    }
+
+    public boolean delete(Long id) 
+    {
+        Optional<Participantes> optionalParticipante = participanteRepository.findById(id);
+
+        if (optionalParticipante.isPresent()) 
+        {
+            participanteRepository.delete(optionalParticipante.get());
+            return true;
+        }
+
+        return false;
+    }
 }
-
-
